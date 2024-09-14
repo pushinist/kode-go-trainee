d6756411ed90061942d88388bade8ab17bdae150
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-09-15 01:04:54 +0200

    note struct and its CRUD operations

---
+++ note.go
@@ -0,0 +1,53 @@
+package main
+
+import "log"
+
+type Note struct {
+	ID      int
+	Title   string
+	Content string
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateNote(title, content string) {
+	query := `INSERT INTO notes (title, content) VALUES (?, ?)`
+	_, err := db.Exec(query, title, content)
+	checkErr(err)
+}
+
+func GetNotes() ([]Note, error) {
+	rows, err := db.Query("SELECT id, title, content FROM notes")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var notes []Note
+
+	for rows.Next() {
+		var note Note
+		err = rows.Scan(&note.ID, &note.Title, &note.Content)
+		if err != nil {
+			return nil, err
+		}
+		notes = append(notes, note)
+	}
+	return notes, nil
+}
+
+func UpdateNotes(id int, title, content string) {
+	query := `UPDATE notes SET title = ?, content = ? WHERE id = ?`
+	_, err := db.Exec(query, title, content, id)
+	checkErr(err)
+}
+
+func DeleteNote(id int) {
+	query := `DELETE FROM notes WHERE id = ?`
+	_, err := db.Exec(query, id)
+	checkErr(err)
+}
