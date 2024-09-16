9c5eca14885b5487cae255e1054ab94e3470e909
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-09-16 10:41:58 +0200

    move notes' handlers to their file

--- note.go
+++ note.go
@@ -1,6 +1,13 @@
 package main
 
-import "log"
+import (
+	"database/sql"
+	"errors"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 type Note struct {
 	ID      int
@@ -21,26 +28,25 @@ func CreateNote(title, content string, userIDInt int) {
 	checkErr(err)
 }
 
-func GetNotes() ([]Note, error) {
-	rows, err := db.Query("SELECT id, title, content FROM notes")
+func GetNotes(userID int) ([]Note, error) {
+	rows, err := db.Query("SELECT id, title, content FROM notes WHERE user_id = ?", userID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
-
 	var notes []Note
-
 	for rows.Next() {
 		var note Note
-		err = rows.Scan(&note.ID, &note.Title, &note.Content)
-		if err != nil {
+		if err = rows.Scan(&note.ID, &note.Title, &note.Content); err != nil {
 			return nil, err
 		}
+		note.UserID = userID
 		notes = append(notes, note)
 	}
 	return notes, nil
 }
 
+// На будущее, для возможности редактировать заметки
 func UpdateNotes(id int, title, content string) {
 	query := `UPDATE notes SET title = ?, content = ? WHERE id = ?`
 	_, err := db.Exec(query, title, content, id)
@@ -51,4 +57,64 @@ func DeleteNote(id int) {
 	query := `DELETE FROM notes WHERE id = ?`
 	_, err := db.Exec(query, id)
 	checkErr(err)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("session_token")
+	var username string
+	var userID int
+	if err == nil {
+		username = sessions[cookie.Value]
+		row := db.QueryRow("SELECT id FROM users WHERE username = ?", username)
+		err = row.Scan(&userID)
+	}
+	notes, err := GetNotes(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	err = tmpl.Execute(w, PageData{
+		Username: username,
+		Notes:    notes,
+	})
+	if err != nil {
+		http.Error(w, "Unable to render page", http.StatusInternalServerError)
+	}
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		title := r.FormValue("title")
+		content := r.FormValue("content")
+		cookie, err := r.Cookie("session_token")
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		username := sessions[cookie.Value]
+		row := db.QueryRow("SELECT id FROM users WHERE username = ?", username)
+		var userID int
+		err = row.Scan(&userID)
+		if err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				http.Error(w, "user not found", http.StatusUnauthorized)
+				return
+			}
+			http.Error(w, "error scanning row", http.StatusInternalServerError)
+			return
+		}
+		CreateNote(title, content, userID)
+		http.Redirect(w, r, "/notes", http.StatusSeeOther)
+	}
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.FormValue("id"))
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
+
+	DeleteNote(id)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
