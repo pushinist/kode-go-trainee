a0b07dba3cfe3348e86a87741617df89416f5d4b
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-09-16 03:07:37 +0200

    add users table

--- database.go
+++ database.go
@@ -12,16 +12,27 @@ var db *sql.DB
 func initDB() {
 	var err error
 
-	db, err = sql.Open("sqlite3", "./notes.db")
+	db, err = sql.Open("sqlite3", "./app.db")
 	if err != nil {
 		log.Fatal(err)
 	}
-	createTable := `
+	createNotesTable := `
 CREATE TABLE IF NOT EXISTS notes (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 title TEXT,
-content TEXT);`
-	_, err = db.Exec(createTable)
+content TEXT,
+user_id INTEGER,
+FOREIGN KEY (user_id) REFERENCES users(id));`
+
+	createUsersTable := `
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT);
+`
+
+	_, err = db.Exec(createNotesTable)
+	_, err = db.Exec(createUsersTable)
 	if err != nil {
 		log.Fatal(err)
 	}
